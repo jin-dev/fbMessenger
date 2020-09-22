8b45590fb2559bc58cd0757ca05e0d1024014cb8
jin <jin@jinui-iMac.local>
2020-09-22 19:09:03 +0900

    updataed main structure

--- fb_messenger_clone/src/App.js
+++ fb_messenger_clone/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import {Button, FormControl, InputLabel, Input } from '@material-ui/core/';
+import Message from './Message';
 
 function App() {
+  const [ input, setInput ] = useState('');
+  const [ messages, setMessages] = useState([{username: 'jin', text: 'hey there'}, {username:'sophia', text: 'hi jin!'}]);
+  const [username, setUsername] = useState('');
+
+//  useState = variable in React;
+// useEffect = run code on a condition in React
+
+  useEffect( () => {
+    //run code here
+    // if its blank inside [], this code runs once when the app component loads
+    // if we have a variable like input, it runs every time input change.
+
+    //const name = prompt('Please enter your name');
+
+    setUsername(prompt('Please enter your name'))
+  }, [] ) // condition;
+
+  
+  
+  const sendMessage = (event) => {
+    event.preventDefault();
+      setMessages([...messages, { username: username, text: input } ]);
+      setInput('');
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> HAHA </h1>
+        <h2> Welcom {username} </h2>
+      <form>
+        <FormControl>
+          
+          <InputLabel>Enter a message...</InputLabel>
+         <input value={input} onChange={event => setInput(event.target.value)} />
+         <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}> Send Message</Button>
+        </FormControl>
+      </form>
+    
+      {
+        messages.map(message => (
+          <Message username={username} message={message} />
+        ))
+      }
     </div>
   );
 }
---
+++ fb_messenger_clone/src/Message.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Card, CardContent, Typography} from "@material-ui/core";
+import './Message.css';
+
+
+function Message(message, username) {
+        const isUser = username === message.username;
+
+        return (
+      
+            <Card className={`message ${isUser && 'message__user'}`}>
+                <CardContent > 
+                <Typography 
+                color="white"
+                variant="h5"
+                component="h2"
+                >
+                {message.username} : {message.message.text}
+                </Typography>
+
+                </CardContent>
+            </Card>
+       
+    )
+}
+
+export default Message
